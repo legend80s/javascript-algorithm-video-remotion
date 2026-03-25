6024b56fcde7ac0048ea527bcaab4d9fbb9d5d6f
legend80s <liuchzong@qq.com>
2026-03-25 20:28:18 +0800

    feat(bubble-sort): 不交换也有音效

--- src/SoundEffects.tsx
+++ src/SoundEffects.tsx
@@ -17,11 +17,18 @@ export const SoundEffects: React.FC<SoundEffectsProps> = ({
 
   steps.forEach((step, stepIndex) => {
     const stepFrame = sortStart + stepIndex * framesPerStep
+    const nextStep = steps[stepIndex + 1]
 
     if (step.swapping.length > 0) {
       audioElements.push(
         <Sequence key={`swap-${stepFrame}`} from={stepFrame} layout="none">
           <Audio src={staticFile("sfx/notification-pop.mp3")} volume={0.5} />
+        </Sequence>,
+      )
+    } else if (step.comparing.length > 0 && !nextStep?.swapping.length) {
+      audioElements.push(
+        <Sequence key={`no-swap-${stepFrame}`} from={stepFrame} layout="none">
+          <Audio src={staticFile("sfx/back-001.mp3")} volume={0.4} />
         </Sequence>,
       )
     }
